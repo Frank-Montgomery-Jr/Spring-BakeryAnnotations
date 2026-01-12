364d3eb544acdda2b2a8f92a6a5f7e5704a4b51e
FrankMontgomery063 <montgomeryfrank.fm@gmail.com>
2026-01-11 21:24:49 -0500

    "muffin service.java"

--- src/main/java/com/zipcodewilmington/bakery/services/MuffinService.java
+++ src/main/java/com/zipcodewilmington/bakery/services/MuffinService.java
@@ -2,34 +2,34 @@ package com.zipcodewilmington.bakery.services;
 
 import com.zipcodewilmington.bakery.models.Muffin;
 import com.zipcodewilmington.bakery.repositories.MuffinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MuffinService {
-    private MuffinRepository repository;
+    
+    @Autowired
+    private MuffinRepository muffinRepository;
 
-    public MuffinService(MuffinRepository repository) {
-        this.repository = repository;
+    public Muffin create(Muffin muffin) {
+        return muffinRepository.save(muffin);
     }
 
-    public Iterable<Muffin> index() {
-        return repository.findAll();
+    public Muffin read(Long id) {
+        return muffinRepository.findById(id).get();
     }
 
-    public Muffin show(Long id) {
-        return repository.findById(id).get();
-    }
-
-    public Muffin create(Muffin baker) {
-        return repository.save(baker);
+    public Iterable<Muffin> readAll() {
+        return muffinRepository.findAll();
     }
 
     public Muffin update(Long id, Muffin newMuffinData) {
-        Muffin originalMuffin = repository.findById(id).get();
+        Muffin originalMuffin = read(id);
         originalMuffin.setFlavor(newMuffinData.getFlavor());
-        return repository.save(originalMuffin);
+        return muffinRepository.save(originalMuffin);
     }
 
-    public Boolean delete(Long id) {
-        repository.deleteById(id);
-        return true;
+    public void delete(Long id) {
+        muffinRepository.deleteById(id);
     }
 }

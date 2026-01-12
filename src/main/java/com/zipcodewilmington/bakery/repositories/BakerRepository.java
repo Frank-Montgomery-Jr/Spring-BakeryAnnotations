0ed6b1ed4fd85394754e435106ee8ddce793cad7
FrankMontgomery063 <montgomeryfrank.fm@gmail.com>
2026-01-11 21:37:37 -0500

    "repaired baker repository"

--- src/main/java/com/zipcodewilmington/bakery/repositories/BakerRepository.java
+++ src/main/java/com/zipcodewilmington/bakery/repositories/BakerRepository.java
@@ -1,9 +1,9 @@
 package com.zipcodewilmington.bakery.repositories;
 
-import com.zipcodewilmington.bakery.models.Muffin;
+import com.zipcodewilmington.bakery.models.Baker;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BakerRepository extends CrudRepository<Muffin, Long> {
+public interface BakerRepository extends CrudRepository<Baker, Long> {
 }
